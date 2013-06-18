aef729dde4bc0f4f63878c84497d6dd3117d3b4f
Louis <louisreid@gmail.com>
2013-06-18 10:18:45 +0100

    buttons update

--- wp-content/themes/legalwrite/footer.php
+++ wp-content/themes/legalwrite/footer.php
@@ -5,12 +5,12 @@
 		<div class="container">
 		 	<div class="row">
 		 		<div class="span12 offset1">
-			       	<a href="/" id="ft_btn" class="btn btn-info">Home</a> 
-			       	<a href="/about-us/" class="btn btn-info">About Us</a> 
-			       	<a href="/clients/" class="btn btn-info">Clients</a> 
-			       	<a href="/courses/" class="btn btn-info">Courses</a> 
-			       	<a href="/drafting-services/" class="btn btn-info">Drafting Services</a> 
-			       	<a href="/contact-us/" class="btn btn-info">Contact Us</a> 
+			       	<a href="/" id="ft_btn" class="btn btn-inverse">Home</a> 
+			       	<a href="/about-us/"id="ft_btn" class="btn btn-inverse">About Us</a> 
+			       	<a href="/clients/"id="ft_btn" class="btn btn-inverse">Clients</a> 
+			       	<a href="/courses/"id="ft_btn" class="btn btn-inverse">Courses</a> 
+			       	<a href="/drafting-services/"id="ft_btn" class="btn btn-inverse">Drafting Services</a> 
+			       	<a href="/contact-us/"id="ft_btn" class="btn btn-inverse">Contact Us</a> 
 		       	</div>
 			</div>
 		</div>
